2f1c3a86d7fa73f347a953dfe5ada4d0d39324f6
Chaniel <Chaniel@users.noreply.github.com>
2014-07-31 10:26:01 +0800

    Update stst.sh

--- stst.sh
+++ stst.sh
@@ -3,3 +3,7 @@ date=`date +%F' '%H:%M:%S`
 
 # search
 echo $date "search ES connections:" `netstat -an | grep 7190 | grep ES | wc -l` >> /data/om/stat.txt
+
+
+# crontab -e
+# */15 * *　× ×  /data/om/stat.sh
